4798c36a86c8459240009160b290963adfb542cc
Jawadul Hasan <jhsowmik35@gmail.com>
2025-08-18 20:44:23 +0600

    Add files via upload

---
+++ Climbing_Stairs.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n == 0) return 1;
+        if(n == 1) return 1;
+        vector<int> dp(n + 1);
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
+
+int main() {
+    Solution sl;
+    int n = 3;
+    cout << "Number of ways to climb " << n << " stairs: " << sl.climbStairs(n) << endl;
+    return 0;
+}
